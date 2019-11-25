8bcfe4abdd3af19c05c4fe2f51a5bf98319f84aa
Tomato830 <jim001fan@gmail.com>
2019-11-25 20:54:20 +0800

    add fragment

---
+++ app/src/main/java/com/tomato830/note_fjm/checkIN.java
@@ -0,0 +1,31 @@
+package com.tomato830.note_fjm;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class checkIN extends Fragment {
+
+
+    public checkIN() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check_in, container, false);
+    }
+
+}
