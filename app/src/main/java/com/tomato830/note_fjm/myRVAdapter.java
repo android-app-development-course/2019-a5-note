60811fee51d454017bb143f9d6061608851ecb28
tjh4886 <863802398@qq.com>
2019-11-27 10:55:24 +0800

    update

---
+++ app/src/main/java/com/tomato830/note_fjm/myRVAdapter.java
@@ -0,0 +1,54 @@
+package com.tomato830.note_fjm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class myRVAdapter extends RecyclerView.Adapter<myRVAdapter.myTVHolder> {
+
+    private final String[] mArray;
+    private final LayoutInflater mLayoutInflater;
+    private final Context mContext;
+
+    public myRVAdapter(Context context){
+        mArray = context.getResources().getStringArray(R.array.testArray);
+        mLayoutInflater = LayoutInflater.from(context);
+        mContext = context;
+    }
+
+
+    @NonNull
+    @Override
+    public myRVAdapter.myTVHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return new myTVHolder(mLayoutInflater.inflate(R.layout.item_cardview,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myTVHolder holder, int position) {
+        holder.mTextview.setText(mArray[position]);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mArray == null ? 0:mArray.length;
+    }
+
+    public class myTVHolder extends RecyclerView.ViewHolder{
+        TextView mTextview;
+
+        public myTVHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextview = (TextView) itemView.findViewById(R.id.cardtext);
+        }
+
+
+
+    }
+}
--- app/src/main/java/com/tomato830/note_fjm/todo.java
+++ app/src/main/java/com/tomato830/note_fjm/todo.java
@@ -3,16 +3,28 @@ package com.tomato830.note_fjm;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+//import com.tomato830.note_fjm.noteUtil.NormalRecyclerViewAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
+
+
+
+
 public class todo extends Fragment {
 
 
@@ -28,4 +40,23 @@ public class todo extends Fragment {
         return inflater.inflate(R.layout.fragment_todo, container, false);
     }
 
+    RecyclerView recyclerView;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+        recyclerView = getActivity().findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        //recyclerView.setAdapter(new NormalRecyclerViewAdapter(getContext()));
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id =item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
